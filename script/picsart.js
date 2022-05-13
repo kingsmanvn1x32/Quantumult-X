e9d15d3027e5650a41653a5e3dbfa03c70008d29
hcsavn <61853235+hcsavn@users.noreply.github.com>
2022-05-14 00:06:48 +0700

    Update picsart.js

--- script/picsart.js
+++ script/picsart.js
@@ -1,14 +1,32 @@
 var hcsavn = JSON.parse($response.body);
 
-hcsavn.status = "success"
-hcsavn.response.original_order_id = "000000000000001"
-hcsavn.response.order_id = "000000000000001"
-hcsavn.response.status = "SUBSCRIPTION_PURCHASED"
-hcsavn.response.app = "com.picsart.studio"
-hcsavn.response.is_trial = false
-hcsavn.response.subscription_id = "com.picsart.studio.subscription_plus_yearly"
-hcsavn.response.purchase_date = 970621200000
-hcsavn.response.expire_date = 253394614800000
-hcsavn.reason = "ok"
-
+hcsavn = {
+  "status": "success",
+  "response": [
+    {
+      "is_eligible_for_grant": true,
+      "limitation": {
+        "max_count": 10000000000000000000000000000,
+        "limits_exceeded": false
+      },
+      "expire_date": 253394614800000,
+      "order_id": "000000000000001",
+      "purchase_date": 1652461282000,
+      "original_order_id": "000000000000001",
+      "reason": "ok",
+      "is_eligible_for_introductory": false,
+      "subscription_id": "com.picsart.studio.subscription_plus_yearly",
+      "is_trial": true,
+      "status": "SUBSCRIPTION_PURCHASED",
+      "plan_meta": {
+        "product_id": "subscription_plus_yearly",
+        "id": "com.picsart.studio.subscription_plus_yearly",
+        "description": "plus",
+        "frequency": "yearly",
+        "type": "renewable",
+        "scope_id": "full"
+      }
+    }
+  ]
+}
 $done({body: JSON.stringify(hcsavn)});
