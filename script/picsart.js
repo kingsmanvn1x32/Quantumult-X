d4d7cb9ef929fb670a960eb06494cf6950087bf3
hcsavn <61853235+hcsavn@users.noreply.github.com>
2022-05-22 19:49:39 +0700

    Update picsart.js

--- script/picsart.js
+++ script/picsart.js
@@ -1,29 +1,5 @@
-{
-  "status": "success",
-  "response": [
-    {
-      "is_eligible_for_grant": true,
-      "limitation": {
-        "max_count": 10,
-        "limits_exceeded": false
-      },
-      "expire_date": 4082375047000,
-      "order_id": "510000939294967",
-      "purchase_date": 1652461282000,
-      "original_order_id": "510000939294967",
-      "reason": "ok",
-      "is_eligible_for_introductory": false,
-      "subscription_id": "com.picsart.studio.subscription_plus_yearly",
-      "is_trial": true,
-      "status": "SUBSCRIPTION_PURCHASED",
-      "plan_meta": {
-        "product_id": "subscription_plus_yearly",
-        "id": "com.picsart.studio.subscription_plus_yearly",
-        "description": "plus",
-        "frequency": "yearly",
-        "type": "renewable",
-        "scope_id": "full"
-      }
-    }
-  ]
-}
+var fbiossdk = JSON.parse($response.body);
+
+fbiossdk.subscription.granted = true
+
+$done({body: JSON.stringify(fbiossdk)});
